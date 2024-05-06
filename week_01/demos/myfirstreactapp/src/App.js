27f05ae458aa1f3037657173c4da5ef93483ef57
pgm-lorevand <lorevand25@student.arteveldehs.be>
2024-05-06 11:12:49 +0200

    demo component

--- week_01/demos/myfirstreactapp/src/App.js
+++ week_01/demos/myfirstreactapp/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import TeamCard from './components/TeamCard';
 
 // dit zien we, als je hier iets aanpast wordt het aangepast in de browser
 function App() {
   return (
     // dit is jsx
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TeamCard jobFunction="CEO" firstName="Lore" lastName="VDA" />
+      <TeamCard jobFunction="teacher" firstName="mark" lastName="langen" />
+      <TeamCard jobFunction="CEO" firstName="Lore" lastName="VDA" />
     </div>
   );
 }
