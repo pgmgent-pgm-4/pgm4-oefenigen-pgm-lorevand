ead576b7595d4be6a050650c2487fdbfbc748cf8
pgm-lorevand <lorevand25@student.arteveldehs.be>
2024-05-06 11:18:22 +0200

    demo w4

--- week_04/demos/context-customhooks/src/context/ThemeContext.js
+++ week_04/demos/context-customhooks/src/context/ThemeContext.js
@@ -1,8 +1,9 @@
-import { createContext, useState } from "react";
+import {  createContext, useState } from "react";
 
 export const ThemeContext = createContext();
 
 export const ThemeArea = ({ children }) => {
+	
 	const [isDarkMode, setIsDarkMode] = useState(false);
 	return (
 		<ThemeContext.Provider value={[isDarkMode, setIsDarkMode]}>
